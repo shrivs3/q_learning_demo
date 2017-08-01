286039a21ba53056f06a68d1feeb7b54be7905f2
shrivs3 <shrivs3@rpi.edu>
2017-08-02 02:31:52 +0530

    added comments

--- Learner.py
+++ Learner.py
@@ -3,27 +3,39 @@ import World
 import threading
 import time
 
+
+##-------------------------------------------------
+# Initializing the game world and the Q matrix
+
 discount = 0.3
 #importing just possible actions in World
 actions = World.actions
 states = []
 Q = {}
+
+#x and y are 2 variables set in World
 for i in range(World.x):
     for j in range(World.y):
         states.append((i, j))
+# states just has all the possible positions as tuples of x,y
 
+#for each tuple, here box, the cost or Q value of choosing an action has been given as 0.1
 for state in states:
     temp = {}
     for action in actions:
         temp[action] = 0.1
         World.set_cell_score(state, action, temp[action])
     Q[state] = temp
+#the state, action and the cost has been stored in the set_cell_store in the World variable
 
+# For the special boxes, different Q values are stored.
 for (i, j, c, w) in World.specials:
     for action in actions:
         Q[(i, j)][action] = w
         World.set_cell_score((i, j), action, w)
 
+##-------------------------------------------------
+# Running the function
 
 def do_action(action):
     s = World.player
@@ -42,7 +54,8 @@ def do_action(action):
     r += World.score
     return s, action, r, s2
 
-
+# for a given postition or state s, it gives us the action which results in the 
+# maximum reward and the reward value.
 def max_Q(s):
     val = None
     act = None
@@ -66,8 +79,11 @@ def run():
     t = 1
     while True:
         # Pick the right action
+        # World.player is the starting position of the player / Starting state
         s = World.player
+        # get the maximum reward with the action from the state s
         max_act, max_val = max_Q(s)
+        #returns the variables after executing the action
         (s, a, r, s2) = do_action(max_act)
 
         # Update Q
--- World.py
+++ World.py
@@ -6,16 +6,17 @@ triangle_size = 0.1
 cell_score_min = -0.2
 cell_score_max = 0.2
 Width = 100
-(x, y) = (5, 5)
+(x, y) = (5,8)
 actions = ["up", "down", "left", "right"]
 
 board = Canvas(master, width=x*Width, height=y*Width)
-player = (0, y-1)
+player = (0,3)
 score = 1
 restart = False
 walk_reward = -0.04
 
-walls = [(1, 1), (1, 2), (2, 1), (2, 2)]
+walls = [(1, 1), (1, 2), (2,6),(3,6),(3,5), (0,4)]
+# specials are tje uniquely valued cells
 specials = [(4, 1, "red", -1), (4, 0, "green", 1)]
 cell_scores = {}
 
